8eb9f191a114335b6aebaef8521c646c3ec1d7ad
Mostafa Zamani <mostafa.zamani88@gmail.com>
2017-05-13 18:22:57 +0430

    Add files via upload
    first commit

---
+++ bazinama.cpp
@@ -0,0 +1,23 @@
+#include "bazinama.h"
+#include <stdio.h>
+
+
+
+bazinama::bazinama()
+{
+}
+
+
+bazinama::~bazinama()
+{
+}
+
+void bazinama::testCrash()
+{
+// 	int b = 12;
+// 	int c = 12;
+// 	int a = b - c;
+// 	printf("%d",1/a);
+	int a = 0;
+	printf("%d",1/a);
+}

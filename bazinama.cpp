22daff9c23747b7e338f07fd4285cebbc685302b
Mostafa <mostafa.zamani88@gmail.com>
2017-05-14 16:11:39 +0430

    Change crash code to 12

--- bazinama.cpp
+++ bazinama.cpp
@@ -1,8 +1,6 @@
 #include "bazinama.h"
 #include <stdio.h>
 
-
-
 bazinama::bazinama()
 {
 }
@@ -14,10 +12,11 @@ bazinama::~bazinama()
 
 void bazinama::testCrash()
 {
-// 	int b = 12;
-// 	int c = 12;
-// 	int a = b - c;
-// 	printf("%d",1/a);
-	int a = 0;
+	int b = 12;
+	int c = 12;
+	int a = b - c;
 	printf("%d",1/a);
+// 	int a = 0;
+// 	printf("%d",1/a);
 }
+

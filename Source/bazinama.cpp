1a3677d0cca40484d536be8d7e895bc497ba13e3
Mostafa <mostafa.zamani88@gmail.com>
2017-05-21 17:07:59 +0430

    Adding delete reference crash code snipet

--- Source/bazinama.cpp
+++ Source/bazinama.cpp
@@ -1,5 +1,6 @@
 #include "bazinama.h"
 #include <stdio.h>
+#include <QString>
 
 bazinama::bazinama()
 {
@@ -12,10 +13,10 @@ bazinama::~bazinama()
 
 void bazinama::testCrash()
 {
-	int b = 12;
-	int c = 12;
-	int a = b - c;
-	printf("%d",1/a);
-	//delete reinterpret_cast<QString*>(0xFEE1DEAD);
+// 	int b = 12;
+// 	int c = 12;
+// 	int a = b - c;
+// 	printf("%d",1/a);
+	delete reinterpret_cast<QString*>(0xFEE1DEAD);
 }
 
